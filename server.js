2a9d1c2943bcec45e34ed17b3f836205da7c208a
Zoriana27 <zad27@cam.ac.uk>
2026-07-03 22:05:57 +0100

    parsing PDFs - check

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const {PDFParse} = require('pdf-parse');
+const fs = require('fs/promises');
+
+
 const app = express();
 const port = 3000;
 
@@ -11,11 +15,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });//__dirname just means "the folder this script is in."
 
-app.post('/upload', upload.single('lectureSlides'), (req, res) =>{
+app.post('/upload', upload.single('lectureSlides'), async (req, res) =>{
     console.log('File received succesfully');
     console.log(req.file);
-    res.send('File uploaded successfully');
+    const t = await parsePdf(req.file.path);
+    console.log(t);
+    res.send('File uploaded and parsed successfully');
+    
 })
+
+async function parsePdf(filePath) {
+  const buffer = await fs.readFile(filePath);
+  const parser = new PDFParse({ data: buffer });
+  const result = await parser.getText();
+  await parser.destroy();
+  return result.text;
+}
 app.listen(port, () => {
     console.log('Server running at http://localhost:${port}');
 
